21ac6d57acc19834f03ad0cbe8534cd6c6d7f98e
siddhantkrishna <siddhantkrishnaaa@gmail.com>
2026-09-13 00:30:16 +0530

    feat: wire organizer command into CLI

--- pytoolkit/cli.py
+++ pytoolkit/cli.py
@@ -1,9 +1,16 @@
 import click
+from tools.organizer import organize_folder
 
 @click.group()
 def main():
     """PyToolkit: A collection of automation tools."""
     pass
+
+@main.command()
+@click.argument('folder')
+def organize(folder):
+    """Organize files in FOLDER by extension."""
+    organize_folder(folder)
 
 if __name__ == "__main__":
     main()
