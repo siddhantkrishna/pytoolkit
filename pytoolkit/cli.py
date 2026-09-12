014cde7d85eccbc04008745ccf57021ef509e884
siddhantkrishna <siddhantkrishnaaa@gmail.com>
2026-09-13 00:31:17 +0530

    feat: wire renamer command into CLI

--- pytoolkit/cli.py
+++ pytoolkit/cli.py
@@ -1,5 +1,6 @@
 import click
 from tools.organizer import organize_folder
+from tools.renamer import rename_files
 
 @click.group()
 def main():
@@ -11,6 +12,16 @@ def main():
 def organize(folder):
     """Organize files in FOLDER by extension."""
     organize_folder(folder)
+
+@main.command()
+@click.argument('folder')
+@click.option('--prefix', default="", help="Prefix to add to filenames.")
+@click.option('--suffix', default="", help="Suffix to add to filenames.")
+@click.option('--ext', default=None, help="Only rename files with this extension.")
+@click.option('--dry-run', is_flag=True, help="Preview changes without renaming.")
+def rename(folder, prefix, suffix, ext, dry_run):
+    """Bulk rename files in FOLDER."""
+    rename_files(folder, prefix, suffix, ext, dry_run)
 
 if __name__ == "__main__":
     main()
